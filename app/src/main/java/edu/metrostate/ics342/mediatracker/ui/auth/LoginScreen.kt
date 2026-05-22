073150096f7dcf100813b8e9d59ee7bdbdd04240
diego <dgodinez227@gmail.com>
2026-05-21 20:41:47 -0500

    Update app theme.

--- app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/LoginScreen.kt
+++ app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/LoginScreen.kt
@@ -9,11 +9,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -122,3 +124,4 @@ fun LoginScreen(
         }
     }
 }
+
