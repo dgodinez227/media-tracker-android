a87f63496e9c83e5bf3396d2d81ec621a6a82c33
diego <dgodinez227@gmail.com>
2026-06-18 21:06:16 -0500

    Week 05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,16 @@
 import com.android.build.api.dsl.ApplicationExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+val localProperties = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) load(file.inputStream())
 }
 
 extensions.configure<ApplicationExtension> {
@@ -35,6 +42,7 @@ extensions.configure<ApplicationExtension> {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -60,9 +68,8 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.retrofit)
-    implementation(libs.retrofit.converters.kotlinx.serializtion)
-    implementation(libs.kotlinx.serialization)
-    implementation(libs.okhttp)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.coil.compose)
     implementation(libs.androidx.datastore.preferences)
---
+++ app/src/main/java/edu/metrostate/ics342/mediatracker/data/network/ApiConstants.kt
@@ -0,0 +1,7 @@
+package edu.metrostate.ics342.mediatracker.data.network
+
+object ApiConstants {
+    const val BASE_URL = "https://wjtzkgpxmxtzcczzbvrz.supabase.co/functions/v1/"
+    val CLIENT_ID: String = "ics342-diego-godinez"
+    val CLIENT_SECRET: String = "mt-bpDYQU9cfMWFuLq8Du"
+}
